62dbd4a920962f399275dceca072fbba5947b4f3
poormonitor <poormonitor@outlook.com>
2021-02-25 08:25:20 +0800

    UPDATE DETAILS

--- main.py
+++ main.py
@@ -8,17 +8,25 @@ import names
 import subprocess
 
 
+def replace_last(string, find, replace):
+    reversed = string[::-1]
+    replaced = reversed.replace(find[::-1], replace[::-1], 1)
+    return replaced[::-1]
+
+
 def gen(exe):
     genin = subprocess.run("python ran.py", shell=True, capture_output=True).stdout.decode()
-    genout = subprocess.run(exe, input=bytes(str(genin), encoding="UTF-8"), shell=True, capture_output=True).stdout.decode()
+    genin = replace_last(genin, "\n", "")
+    genout = subprocess.run(exe, input=bytes(str(genin), encoding="UTF-8"), shell=True,
+                            capture_output=True).stdout.decode()
     return genin, genout
 
 
 def genfile(cmd):
     name = names.get_last_name().lower()
     genin, genout = gen(cmd)
-    filein = open(name + ".in", 'w')
-    fileout = open(name + ".out", "w")
+    filein = open("code/" + name + ".in", 'w')
+    fileout = open("code/" + name + ".out", "w")
     filein.write(genin)
     fileout.write(genout)
     filein.close()
@@ -32,5 +40,7 @@ if __name__ == "__main__":
         times = 20
     else:
         times = int(times)
+    if cmd == "":
+        cmd = "python code.py"
     for i in range(times):
         genfile(cmd)

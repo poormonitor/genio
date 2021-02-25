b59df380331727b705e00393ca4c7ccca14ab472
poormonitor <poormonitor@outlook.com>
2021-02-25 14:47:46 +0800

    update main.py for better output

--- main.py
+++ main.py
@@ -6,27 +6,30 @@
 
 import names
 import subprocess
+import os
+import platform
 
 
-def replace_last(string, find, replace):
-    reversed = string[::-1]
-    replaced = reversed.replace(find[::-1], replace[::-1], 1)
-    return replaced[::-1]
+def pythoncmd():
+    if platform.system() == "Linux":
+        return "python3"
+    else:
+        return "python"
 
 
 def gen(exe):
-    genin = subprocess.run("python ran.py", shell=True, capture_output=True).stdout.decode()
-    genin = replace_last(genin, "\n", "")
-    genout = subprocess.run(exe, input=bytes(str(genin), encoding="UTF-8"), shell=True,
-                            capture_output=True).stdout.decode()
+    genin = subprocess.run("%s ran.py" % pythoncmd(), shell=True, capture_output=True).stdout.decode()[::-1].replace(
+        "\n", "", 1).replace("\r", "", 1)[::-1]
+    genout = subprocess.run(exe, input=bytes(genin, encoding="UTF-8"), shell=True,
+                            capture_output=True).stdout.decode()[::-1].replace("\n", "", 1).replace("\r", "", 1)[::-1]
     return genin, genout
 
 
 def genfile(cmd):
     name = names.get_last_name().lower()
     genin, genout = gen(cmd)
-    filein = open("code/" + name + ".in", 'w')
-    fileout = open("code/" + name + ".out", "w")
+    filein = open("io/" + name + ".in", 'w')
+    fileout = open("io/" + name + ".out", "w")
     filein.write(genin)
     fileout.write(genout)
     filein.close()
@@ -34,13 +37,15 @@ def genfile(cmd):
 
 
 if __name__ == "__main__":
-    cmd = input("Input your command (eg. python code.py): ")
+    cmd = input("Input your command (eg. %s code.py): " % pythoncmd())
     times = input("Input pair numbers (default: 20) : ")
     if times == "":
         times = 20
     else:
         times = int(times)
     if cmd == "":
-        cmd = "python code.py"
+        cmd = "%s code.py" % pythoncmd()
+    if not os.path.exists("io"):
+        os.mkdir("io")
     for i in range(times):
         genfile(cmd)
